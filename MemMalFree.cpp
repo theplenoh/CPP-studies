c6c1dab85ff8ed48387261121bb26e7c950293fa
theplenoh <tylrd529@gmail.com>
2022-03-30 11:30:37 +0900

    백견이불여일타

---
+++ MemMalFree.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+char* MakeStrAdr(int len)
+{
+    char* str = (char*)malloc(sizeof(char)*len);
+    return str;
+}
+
+int main()
+{
+    char* str = MakeStrAdr(20);
+    strcpy(str, "I am so happy!");
+    cout<<str<<endl;
+
+    free(str);
+
+    return 0;
+}
---
+++ NewDelete.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+char* MakeStrAdr(int len)
+{
+    char* str = new char[len];
+    return str;
+}
+
+int main()
+{
+    char* str = MakeStrAdr(20);
+    strcpy(str, "I am so happy!");
+    cout<<str<<endl;
+
+    delete []str;
+
+    return 0;
+}
