3769e1a4ca67b8339a5608af01bac2fac9b28243
theplenoh <tylrd529@gmail.com>
2022-04-01 15:23:13 +0900

    문제 02-4 풀이

---
+++ prob02-4-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+
+    for(int i=0; i<5; i++)
+        cout << rand() % 99 + 1 << ' ';
+
+    cout << endl;
+
+    return 0;
+}
